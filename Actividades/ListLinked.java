dd0aa958e6aff83f08865242694b497c042440d3
Leito180404 <leonardo.lonasco@estudiante.ucsm.edu.pe>
2025-06-03 19:22:04 -0500

    Update ListLinked.java

--- Actividades/ListLinked.java
+++ Actividades/ListLinked.java
@@ -1,5 +1,92 @@
 package Actividades;
 
-public class ListLinked {
-    
+public class ListLinked<E> {
+    private static class Node<E> {
+        E data;
+        Node<E> next;
+
+        Node(E data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    private Node<E> head;
+    private int size;
+
+    public ListLinked() {
+        head = null;
+        size = 0;
+    }
+
+    public boolean isEmpty() {
+        return head == null;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public void addFirst(E data) {
+        Node<E> newNode = new Node<>(data);
+        newNode.next = head;
+        head = newNode;
+        size++;
+    }
+
+    public void addLast(E data) {
+        Node<E> newNode = new Node<>(data);
+        if (head == null) {
+            head = newNode;
+        } else {
+            Node<E> current = head;
+            while (current.next != null) {
+                current = current.next;
+            }
+            current.next = newNode;
+        }
+        size++;
+    }
+
+    public E removeFirst() {
+        if (head == null) return null;
+        E data = head.data;
+        head = head.next;
+        size--;
+        return data;
+    }
+
+    public boolean remove(E data) {
+        if (head == null) return false;
+        if (head.data.equals(data)) {
+            head = head.next;
+            size--;
+            return true;
+        }
+        Node<E> current = head;
+        while (current.next != null && !current.next.data.equals(data)) {
+            current = current.next;
+        }
+        if (current.next == null) return false;
+        current.next = current.next.next;
+        size--;
+        return true;
+    }
+
+    public boolean contains(E data) {
+        Node<E> current = head;
+        while (current != null) {
+            if (current.data.equals(data)) return true;
+            current = current.next;
+        }
+        return false;
+    }
+
+    public E get(int index) {
+        if (index < 0 || index >= size) throw new IndexOutOfBoundsException();
+        Node<E> current = head;
+        for (int i = 0; i < index; i++) current = current.next;
+        return current.data;
+    }
 }
+
