6dbd4b5a5ba9e59e1c36bf0857d786e574a985f1
Leito180404 <leonardo.lonasco@estudiante.ucsm.edu.pe>
2025-06-03 19:51:08 -0500

    Update GraphLink.java

--- Actividades/graph/GraphLink.java
+++ Actividades/graph/GraphLink.java
@@ -18,7 +18,14 @@ public class GraphLink<E> {
     public void insertEdge(E verOri, E verDes) {
         Vertex<E> origen = null;
         Vertex<E> destino = null;
-    
+        
+        for (int i = 0; i < listVertex.size(); i++) {
+            Vertex<E> v = listVertex.get(i);
+            if (v.getData().equals(verOri)) {
+                origen = v;
+                break;
+            }
+        }
     }
     public String toString() {
         return this.listVertex.toString();
