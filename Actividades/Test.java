6c6b375c7f8675084a35c76f0a173bb9ef438203
Leito180404 <leonardo.lonasco@estudiante.ucsm.edu.pe>
2025-06-05 22:51:59 -0500

    Update Test.java

--- Actividades/Test.java
+++ Actividades/Test.java
@@ -12,12 +12,21 @@ public class Test {
         graph.insertVertex(3);
         graph.insertVertex(4);
 
-        System.out.println("\ngrafo despues de insertar vertices:");
+        graph.insertEdge(1, 2); // Arista entre 1 y 2
+        graph.insertEdge(2, 3); // Arista entre 2 y 3
+        graph.insertEdge(3, 4); // Arista entre 3 y 4
+
+        System.out.println("\ngrafo despues de insertar vertices y aristas:");
         System.out.println(graph);
-    
+
         System.out.println("\nBuscando el vertice 2: " + graph.searchVertex(2));
         System.out.println("Buscando el vertice 5: " + graph.searchVertex(5));
+
+        System.out.println("\nBuscando arista entre 2 y 3:");
+        System.out.println("Resultado: " + graph.searchEdge(2, 3));
+
+        System.out.println("\nBuscando arista entre 1 y 4:");
+        System.out.println("Resultado: " + graph.searchEdge(1, 4)); 
     }
-        
-        
 }
+
