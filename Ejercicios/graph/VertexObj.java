ebc5423372d93213615a6692c220a24abc05b4eb
Leito180404 <leonardo.lonasco@estudiante.ucsm.edu.pe>
2025-06-08 11:58:07 -0500

    Update VertexObj.java

--- Ejercicios/graph/VertexObj.java
+++ Ejercicios/graph/VertexObj.java
@@ -3,6 +3,7 @@ package Ejercicios.graph;
 public class VertexObj<V, E> {
     protected V info;
     protected int position;
+    public Object secEdge;
 
     public VertexObj(V info, int position) {
         this.info = info;
