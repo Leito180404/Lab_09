1d5d566e70cf24ba456092683a55606c94bb2036
Leito180404 <leonardo.lonasco@estudiante.ucsm.edu.pe>
2025-06-08 11:58:10 -0500

    Update GraphListEdge.java

--- Ejercicios/graph/GraphListEdge.java
+++ Ejercicios/graph/GraphListEdge.java
@@ -5,11 +5,12 @@ import java.util.ArrayList;
 public class GraphListEdge<V, E> {
     ArrayList<VertexObj<V, E>> secVertex;
     ArrayList<EdgeObj<V, E>> secEdge;
-
+    
     public GraphListEdge() {
         this.secVertex = new ArrayList<VertexObj<V, E>>();
-        this.secEdge = new ArrayList<EdgeObj<V, E>>();
+        this.secEdge = new ArrayList<EdgeObj<V, E>>(); 
     }
+
     public void insertVertex(VertexObj<V, E> v) {
         for (VertexObj<V, E> vertex : secVertex) {
             if (vertex.info.equals(v.info)) {
@@ -17,5 +18,17 @@ public class GraphListEdge<V, E> {
             }
         }
         secVertex.add(v);
+    }
+
+    public void insertEdge(VertexObj<V, E> v, VertexObj<V, E> z) {
+        for (EdgeObj<V, E> edge : secEdge) {
+            if (edge.endVertex1.equals(v) && edge.endVertex2.equals(z) || edge.endVertex1.equals(z) && edge.endVertex2.equals(v)) {
+                return;
+            }
+        }
+        EdgeObj<V, E> newEdge = new EdgeObj<>(v, z, null, 0);
+        secEdge.add(newEdge);
+        ((ArrayList<EdgeObj<V, E>>) v.secEdge).add(newEdge);
+        ((ArrayList<EdgeObj<V, E>>) z.secEdge).add(newEdge);
     }
 }
