f46599072cacf1920902feb64a8400c17df3ce1b
Leito180404 <leonardo.lonasco@estudiante.ucsm.edu.pe>
2025-06-07 22:19:14 -0500

    Update GraphLink1.java

--- Ejercicios/GraphLink1.java
+++ Ejercicios/GraphLink1.java
@@ -221,6 +221,41 @@ public class GraphLink1<E> {
         return null;
     }
 
+
+    // Ejercicio 2.a
+    public void insertEdgeWeight(E verOri, E verDes, int w) {
+        Vertex<E> origen = null;
+        Vertex<E> destino = null;
+
+        for (int i = 0; i < listVertex.size(); i++) {
+            Vertex<E> v = listVertex.get(i);
+            if (v.getData().equals(verOri)) {
+                origen = v;
+                break;
+            }
+        }
+
+        for (int i = 0; i < listVertex.size(); i++) {
+            Vertex<E> v = listVertex.get(i);
+            if (v.getData().equals(verDes)) {
+                destino = v;
+                break;
+            }
+        }
+
+        if (origen != null && destino != null) {
+            Edge<E> edge1 = new Edge<>(destino, w);
+            if (!origen.listAdj.contains(edge1)) {
+                origen.listAdj.addLast(edge1);
+            }
+
+            Edge<E> edge2 = new Edge<>(origen, w);
+            if (!destino.listAdj.contains(edge2)) {
+                destino.listAdj.addLast(edge2);
+            }
+        }
+    }
+
     public String toString() {
         return this.listVertex.toString();
     }
