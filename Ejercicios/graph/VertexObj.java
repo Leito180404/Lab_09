e79ddc7e3d4b4d4a17e4d55a1d301ed915cb0155
Leito180404 <leonardo.lonasco@estudiante.ucsm.edu.pe>
2025-06-08 12:10:23 -0500

    Update VertexObj.java

--- Ejercicios/graph/VertexObj.java
+++ Ejercicios/graph/VertexObj.java
@@ -1,14 +1,17 @@
 package Ejercicios.graph;
 
+import java.util.ArrayList;
+
 public class VertexObj<V, E> {
     protected V info;
     protected int position;
-    public Object secEdge;
+    public ArrayList<EdgeObj<V, E>> secEdge;
 
     public VertexObj(V info, int position) {
         this.info = info;
         this.position = position;
+        this.secEdge = new ArrayList<EdgeObj<V, E>>();
     }
-
     // otros métodos
 }
+
