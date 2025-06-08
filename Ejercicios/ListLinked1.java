fb15b92377230ca1092a0ab4ba3058e3a235d7bc
Leito180404 <leonardo.lonasco@estudiante.ucsm.edu.pe>
2025-06-07 23:37:22 -0500

    Create ListLinked1.java

---
+++ Ejercicios/ListLinked1.java
@@ -0,0 +1,144 @@
+package Ejercicios;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+public class ListLinked1<E> implements Iterable<E> {
+    private static class Node<E> {
+        E data;
+        Node<E> next;
+
+        Node(E data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    private Node<E> head;
+    private int size;
+
+    public ListLinked1() {
+        head = null;
+        size = 0;
+    }
+
+    public boolean isEmpty() {
+        return head == null;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public void addFirst(E data) {
+        Node<E> newNode = new Node<>(data);
+        newNode.next = head;
+        head = newNode;
+        size++;
+    }
+
+    public void addLast(E data) {
+        Node<E> newNode = new Node<>(data);
+        if (head == null) {
+            head = newNode;
+        } else {
+            Node<E> current = head;
+            while (current.next != null) {
+                current = current.next;
+            }
+            current.next = newNode;
+        }
+        size++;
+    }
+
+    public E removeFirst() {
+        if (head == null) return null;
+        E data = head.data;
+        head = head.next;
+        size--;
+        return data;
+    }
+
+    public boolean remove(E data) {
+        if (head == null) return false;
+        if (head.data.equals(data)) {
+            head = head.next;
+            size--;
+            return true;
+        }
+        Node<E> current = head;
+        while (current.next != null && !current.next.data.equals(data)) {
+            current = current.next;
+        }
+        if (current.next == null) return false;
+        current.next = current.next.next;
+        size--;
+        return true;
+    }
+
+    public boolean contains(E data) {
+        Node<E> current = head;
+        while (current != null) {
+            if (current.data.equals(data)) return true;
+            current = current.next;
+        }
+        return false;
+    }
+
+    public E get(int index) {
+        if (index < 0 || index >= size) throw new IndexOutOfBoundsException();
+        Node<E> current = head;
+        for (int i = 0; i < index; i++) current = current.next;
+        return current.data;
+    }
+
+    public int indexOf(E data) {
+        Node<E> current = head;
+        int index = 0;
+        while (current != null) {
+            if (current.data.equals(data)) {
+                return index;
+            }
+            current = current.next;
+            index++;
+        }
+        return -1;
+    }
+
+    @Override
+    public Iterator<E> iterator() {
+        return new Iterator<E>() {
+            private Node<E> current = head;
+
+            @Override
+            public boolean hasNext() {
+                return current != null;
+            }
+
+            @Override
+            public E next() {
+                if (!hasNext()) {
+                    throw new NoSuchElementException();
+                }
+                E data = current.data;
+                current = current.next;
+                return data;
+            }
+        };
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder result = new StringBuilder();
+        Node<E> current = head;
+        while (current != null) {
+            result.append(current.data).append(" -> ");
+            current = current.next;
+        }
+        if (result.length() > 0) {
+            result.setLength(result.length() - 4);
+        }
+        return result.toString();
+    }
+}
+
