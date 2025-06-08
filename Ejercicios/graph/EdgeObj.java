cc1933a03b453901ece7a9bfa2dfbc03cd4ade8b
Leito180404 <leonardo.lonasco@estudiante.ucsm.edu.pe>
2025-06-08 11:37:21 -0500

    Update EdgeObj.java

--- Ejercicios/graph/EdgeObj.java
+++ Ejercicios/graph/EdgeObj.java
@@ -1,5 +1,18 @@
 package Ejercicios.graph;
 
-public class EdgeObj {
-    
+public class EdgeObj<V, E> {
+    protected E info;
+    protected VertexObj<V, E> endVertex1;
+    protected VertexObj<V, E> endVertex2;
+    protected int position;
+
+    public EdgeObj(VertexObj<V, E> vert1, VertexObj<V, E> vert2, E info, int position) {
+        this.endVertex1 = vert1;
+        this.endVertex2 = vert2;
+        this.info = info;
+        this.position = position;
+    }
+
+    // otros métodos
 }
+
